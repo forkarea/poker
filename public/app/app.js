18f997b01cae2b4b809a9ec23a58a405ffcc55c8
trzewior <lukasz.trzewik@gorrion.pl>
2015-11-30 17:08:23 +0100

    nav fixes

--- public/app/app.js
+++ public/app/app.js
@@ -18,9 +18,9 @@
 
 		}]);
 
-	app.run(['$rootScope', function ($rootScope) {
-		$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-			$rootScope.title = current.$$route.title;
-		});
-	}]);
+	// app.run(['$rootScope', function ($rootScope) {
+	// 	$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+	// 		$rootScope.title = current.$$route.title;
+	// 	});
+	// }]);
 })();
--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -1,9 +1,12 @@
 (function () {
 	var app = angular.module("gorrionPoker");
-	
-	app.controller("mainController", function($scope, $location){
-		$scope.isActive = function(href){
+
+	app.controller("mainController", function ($scope, $location) {
+
+		$scope.isActive = function (href) {
 			return $location.url() == href ? "active" : "";
 		}
+
+		$scope.isCollapsed = true;
 	});
 })();
